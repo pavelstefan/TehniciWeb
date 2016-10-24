9043463603b1cf03854e2b29f981c9785c6ebe69
Pavel Stefan <pavel_stefan_96@yahoo.ro>
2016-10-24 22:40:03 +0300

    added new pages, improved css and javascript

--- tema1/js/app.js
+++ tema1/js/app.js
@@ -1,6 +1,16 @@
 /*Menu toggle*/
 window.addEventListener("load", function(){
     document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
+    if(window.innerWidth <= 1000)
+        menu.style.zIndex = "-100"; 
+})
+
+window.addEventListener("resize", function(){
+    if(menuVsible && window.innerHeight <= 1000)
+        toggleMenu();
+    if(!menuVsible && window.innerWidth > 1000)
+        toggleMenu();
+    
 })
 
 var menuVsible = false;
@@ -9,6 +19,7 @@ function toggleMenu(){
     var bg = document.getElementById("menu-bg");
     var menu = document.getElementById("menu");
     menuVsible = !menuVsible;
+    menu.style.zIndex = "100";
     
     var timeline = new Timeline(1, 60);
     var timeline2 = new Timeline(1, 60);
@@ -19,6 +30,7 @@ function toggleMenu(){
     var on2 = new KeyFrame("menu-bg");
     on1.addKey("opacity", "1");
     on2.addKey("opacity", "0.5");
+    
     
     var off1 = new KeyFrame("menu-bg");
     var off2 = new KeyFrame("menu-bg");
@@ -42,7 +54,10 @@ function toggleMenu(){
     
     timeline.addKeyframe(frame1);
     timeline2.addKeyframe(frame2);
-    timeline.renderAnimation();
+    timeline.renderAnimation(function(){
+        if(menu.style.opacity == 0)
+            menu.style.zIndex = "-100";
+    });
     timeline2.renderAnimation();
     
 }
